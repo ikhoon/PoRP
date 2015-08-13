95b8521eb760d5f04e3a0a12e8ec3e70a3942c83
Ikhoon Eom <ih.pert@gmail.com>
2015-08-13 23:23:19 +0900

    Week2 - Root solver for 2nd degree polynomial, Spreadsheet-like calculator

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Cousera Site : https://class.coursera.org/reactive-002/assignment
 
 ### Week 1: Quickcheck
 Source code: [quickcheck/QuickCheck.scala](https://github.com/ikhoon/PoRP/blob/master/quickcheck/src/main/scala/quickcheck/QuickCheck.scala)
+
+### Week 2: Calculator
+Source code: [calculator/Calculator.scala](https://github.com/ikhoon/PoRP/blob/master/calculator/src/main/scala/calculator/Calculator.scala), [calculator/TweetLength.scala](https://github.com/ikhoon/PoRP/blob/master/calculator/src/main/scala/calculator/TweetLength.scala)
--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -10,12 +10,17 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
   def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
-  }
+      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = namedExpressions.map {
+        case (k, v) => (k, Signal(eval(v(), namedExpressions)))
+      }
 
-  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
+    case Literal(v) => v
+    case Ref(n) => eval(getReferenceExpr(n, references), references)
+    case Plus(a, b) => eval(a, references) + eval(b, references)
+    case Minus(a, b) => eval(a, references) - eval(b, references)
+    case Times(a, b) => eval(a, references) * eval(b, references)
+    case Divide(a, b) => eval(a, references) / eval(b, references)
   }
 
   /** Get the Expr for a referenced variables.
